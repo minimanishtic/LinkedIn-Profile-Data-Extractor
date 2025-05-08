13b0f6614a53fc6a617ee50e1ee0498b8ffb388c
minimanishtic <manishdwivedi9639@gmail.com>
2025-05-08 11:52:21 +0000

    Improvement A

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 import Home from "./components/home";
 
 function App() {
   return (
     <Suspense fallback={<p>Loading...</p>}>
       <>
+        <Analytics />
         <Routes>
           <Route path="/" element={<Home />} />
           {/* Add this before any catchall route */}
