331ff9558b8892c70d5a6664b71b1c401f63b138
minimanishtic <manishdwivedi9639@gmail.com>
2025-05-19 17:24:51 +0000

    Test 3

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import Home from "./components/home";
 function App() {
   return (
     <Suspense fallback={<p>Loading...</p>}>
-      <>
+      <div>
         <Analytics />
         <Routes>
           <Route path="/" element={<Home />} />
@@ -15,7 +15,7 @@ function App() {
             <Route path="/tempobook/*" />
           )}
         </Routes>
-      </>
+      </div>
     </Suspense>
   );
 }
